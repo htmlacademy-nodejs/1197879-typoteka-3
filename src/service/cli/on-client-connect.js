6b8c8491a1989a203c0cee06905f7939d37e943e
Mike-Cursor <65134423+MikeHTMLCSS@users.noreply.github.com>
2022-02-03 19:13:58 +0300

    Update on-client-connect.js
    Попытался исправить коды ошибки

--- src/service/cli/on-client-connect.js
+++ src/service/cli/on-client-connect.js
@@ -3,6 +3,7 @@
 const HttpCode = {
   OK: 200,
   NOT_FOUND: 404,
+  SERVER_ERROR: 500,
 };
 const MOCK_FILE_NAME = `./src/service/cli/mocks.json`;
 
@@ -32,7 +33,7 @@ module.exports = {
           const mocks = await fs.readFile(MOCK_FILE_NAME);
           sendResponse(res, HttpCode.OK, `<ul>${JSON.parse(mocks).map((post) => `<li>${post.title}</li>`).join(``)}</ul>`);
         } catch (error) {
-          sendResponse(res, HttpCode.NOT_FOUND, `Not found`);
+          sendResponse(res, HttpCode.SERVER_ERROR, `Server side issues`);
         }
         break;
       default:
