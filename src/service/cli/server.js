e710e23baf0f5de72969ab53dae29cbd10c16211
Mike-Cursor <65134423+MikeHTMLCSS@users.noreply.github.com>
2022-02-08 13:11:04 +0300

    Перевод сервера в service с модуля http на express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,22 +1,34 @@
 'use strict';
 
 const DEFAULT_PORT = 3000;
-const Flag = {
-  LISTENING: `listening`,
-  ERROR: `error`,
+const MOCK_FILE_NAME = `./src/service/cli/mocks.json`;
+const HttpCode = {
+  NOT_FOUND: 404,
 };
 
-const chalk = require(`chalk`);
-const http = require(`http`);
-const {onClientConnect} = require(`./on-client-connect.js`);
+const express = require(`express`);
+const {readFile} = require(`./read-file.js`);
+
+const app = express();
+app.use(express.json());
+app.get(`/posts`, async (_req, res) => {
+  try {
+    let mocks = await readFile(MOCK_FILE_NAME);
+    mocks = JSON.parse(mocks);
+    res.json(mocks);
+  } catch (err) {
+    res.send([]);
+  }
+});
+
+app.use((_req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
   run([portArg]) {
     const port = parseInt(portArg, 10) || DEFAULT_PORT;
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(Flag.LISTENING, () => console.info(chalk.green(`Ожидаю соединений ${port}`)))
-      .on(Flag.ERROR, ({message}) => console.error(chalk.red(`Ошибка при создании сервера: ${message}`)));
+    app.listen(port);
   },
 };
